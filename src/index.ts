b17876441eb936cc9993f8aa820f74993eaed0c7
idleberg <git@idleberg.com>
2020-09-07 13:28:21 +0200

    minor adjustments

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,12 @@
 import { TextEditor, Position, window } from 'vscode';
 
 const insertText = (text: string, appendText = false, newLine = false): void => {
-  const activeTextEditor: TextEditor|undefined = window.activeTextEditor;
+  const activeTextEditor: TextEditor | undefined = window.activeTextEditor;
+
   if (!activeTextEditor) return;
 
   activeTextEditor.edit(
-    edit => activeTextEditor.selections.forEach(
+    edit => activeTextEditor.selections.map(
       selection => {
         if (!appendText) edit.delete(selection);
 

fa03ddec2c6a8dbeb97059cea01820e4225e81b3
Jan T. Sott <git@idleberg.com>
2025-10-13 14:57:21 +0200

    chor: use shared config

--- src/index.ts
+++ src/index.ts
@@ -1,29 +1,21 @@
-import { TextEditor, Position, window } from 'vscode';
+import { type Position, type TextEditor, window } from 'vscode';
 
 const insertText = (text: string, appendText = false, newLine = false): void => {
-  const activeTextEditor: TextEditor | undefined = window.activeTextEditor;
+	const activeTextEditor: TextEditor | undefined = window.activeTextEditor;
 
-  if (!activeTextEditor) return;
+	if (!activeTextEditor) return;
 
-  activeTextEditor.edit(
-    edit => activeTextEditor.selections.map(
-      selection => {
-        if (!appendText) edit.delete(selection);
+	activeTextEditor.edit((edit) =>
+		activeTextEditor.selections.forEach((selection) => {
+			if (!appendText) edit.delete(selection);
 
-        const location: Position = appendText
-          ? selection.end
-          : selection.start;
+			const location: Position = appendText ? selection.end : selection.start;
 
-        const value: string = appendText && newLine
-          ? `\n${text}`
-          : text;
+			const value: string = appendText && newLine ? `\n${text}` : text;
 
-        edit.insert(location, value);
-      }
-    )
-  );
+			edit.insert(location, value);
+		}),
+	);
 };
 
-export {
-  insertText
-};
+export { insertText };
